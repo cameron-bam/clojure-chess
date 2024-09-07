95e5f521621068d2fe563511e2e69c8babd93132
Cameron White <cameronwhite16@gmail.com>
2024-09-06 21:01:39 -0600

    moving board to a better namespace

--- src/chess/board.clj
+++ src/chess/board.clj
@@ -1,24 +1,27 @@
-(ns chess.board.core
-  (:require [clojure.string :as str]))
+(ns chess.board)
 
-(def letters (str/upper-case "abcdefgh"))
+(def RANKS "87654321")
+(def FILES "ABCDEFGH")
 
 (defn array-coords->board-coords [[x y]]
-  (apply str (- 8 x) (str (get letters y))))
+  {:rank (str (get RANKS x))
+   :file (str (get FILES y))})
 
-(defn board-coords->rank [coords]
-  (Integer/parseInt (str (get coords 0)) 10))
+(defn ->coords-map [coords]
+  (if (map? coords)
+    coords
+    {:rank (str (.charAt coords 0))
+     :file (str (.charAt coords 1))}))
 
-(defn board-coards->file [coords]
-  (str (get coords 1)))
+(defn board-coords->rank [{:keys [rank]}] rank)
+(defn board-coards->file [{:keys [file]}] file)
 
-(defn board-coords->array-coords [coords]
-  [(try (- 8 (board-coords->rank coords)) (catch Exception _ -1))
-   (try (int (.indexOf (seq letters) (.charAt coords 1))) (catch Exception _ -1))])
+(defn board-coords->array-coords [{:keys [rank file]}]
+  [(try (int (.indexOf (seq RANKS) (.charAt rank 0))) (catch Exception _ -1))
+   (try (int (.indexOf (seq FILES) (.charAt file 0))) (catch Exception _ -1))])
 
 (defn includes [x [start end]]
-  (and (>= x start)
-       (< x end)))
+  (and (>= x start) (< x end)))
 
 (defn array-coords-in-bounds? [coords]
   (when (every? #(includes % [0 8]) coords)
@@ -28,7 +31,7 @@
   (-> coords board-coords->array-coords array-coords-in-bounds?))
 
 (def piece-aliases {:r :rook :kn :knight :b :bishop :q :queen :k :king :p :pawn})
-(def last-ranks {:white 8 :black 1})
+(def last-ranks {:white "8" :black "1"})
 
 (defn alias-vec->pieces [color array]
   (->> array
@@ -47,11 +50,19 @@
                                                  [7 [:p :p :p :p :p :p :p :p]]])
                       (alias-vec->pieces :black [[2 [:p :p :p :p :p :p :p :p]]
                                                  [1 [:r :kn :b :q :k :b :kn :r]]]))))
-
-(def initial-castles {:short true :long true})
+(defn normalize-board [board]
+  (->> board
+       (into {} (map (fn [[k v]]
+                       (if (keyword? k)
+                         (case k
+                           :castling-rights [k (into #{} (map ->coords-map) v)]
+                           :en-passante [k (->coords-map v)] 
+                           [k v])
+                         [(->coords-map k) (update v :coords ->coords-map)]))))))
 
 (def initial-board (-> (reduce (fn [board {:keys [coords] :as piece}] (assoc board coords piece)) {} initial-pieces)
-                       (assoc :turn :white :castling-rights #{"8A" "8E" "8H" "1A" "1E" "1H"})))
+                       (assoc :turn :white :castling-rights #{"8A" "8E" "8H" "1A" "1E" "1H"})
+                       (normalize-board)))
 
 (defn opposing-color [color]
   (if (= color :white) :black :white))
@@ -90,17 +101,19 @@
   (build-moves board start -1 rook-directions))
 
 (comment
-  (get-basic-moves {"1A" {:coords "1A" :piece :rook :color :white}
-                    "2A" {:coords "2A" :piece :pawn :color :white}
-                    "8A" {:coords "8A" :piece :rook :color :black}} "1A"))
+  (get-basic-moves (normalize-board {"1A" {:coords "1A" :piece :rook :color :white}
+                                     "2A" {:coords "2A" :piece :pawn :color :white}
+                                     "8A" {:coords "8A" :piece :rook :color :black}}) (->coords-map "1A")))
 
 (def knight-directions #{[2 1] [2 -1] [-2 1] [-2 -1] [1 2] [1 -2] [-1 2] [-1 -2]})
 (defmethod get-basic-moves :knight [board start]
   (build-moves board start 1 knight-directions))
 
 (comment
-  (get-basic-moves {"8B" {:coords "8B" :piece :knight :color :white}
-                    "7B" {:coords "7B" :piece :pawn :color :white}} "8B"))
+  (get-basic-moves (normalize-board
+                    {"8B" {:coords "8B" :piece :knight :color :white}
+                     "7B" {:coords "7B" :piece :pawn :color :white}})
+                   (->coords-map "8B")))
 
 (def bishop-directions #{[1 1] [1 -1] [-1 1] [-1 -1]})
 (defmethod get-basic-moves :bishop [board start]
@@ -144,7 +157,7 @@
 (defn get-all-basic-moves [board color]
   (->> board
        keys
-       (filter string?)
+       (filter map?)
        (map (partial get board))
        (filter (comp (partial = color) :color))
        (mapcat (comp (partial get-basic-moves board) :coords))))
@@ -163,21 +176,22 @@
         under-attack (->> (get-all-basic-moves board (opposing-color color))
                           (map :finish)
                           set)
-        files-between (fn [start-file finish-file]
-                        (let [direction (->> [finish-file start-file]
-                                             (map (comp second board-coords->array-coords (partial str "1")))
+        files-between (fn [start finish]
+                        (let [piece->file (comp :file :coords)
+                              start-file (piece->file start)
+                              finish-file (piece->file finish)
+                              direction (->> [finish-file start-file]
+                                             (map (comp second board-coords->array-coords (partial assoc {:rank "1"} :file)))
                                              (apply -))
                               direction (/ direction (abs direction))]
-                          (->> (iterate #(traverse-step % [0 direction]) (str "1" start-file))
-                               (map board-coards->file)
+                          (->> (iterate #(traverse-step % [0 direction]) {:rank "1" :file start-file})
+                               (map :file)
                                (take-while (complement #{finish-file})))))
-        castles (update castles :king first)
+        {:keys [king] :as castles} (update castles :king first)
         castles (update castles :rook
                         (partial keep (fn [rook]
-                                        (let [castle-spaces (->> (files-between
-                                                                  (-> castles :king :coords board-coards->file)
-                                                                  (-> rook :coords board-coards->file))
-                                                                 (map (fn [file] (-> rook :coords board-coords->rank (str file)))))]
+                                        (let [castle-spaces (->> (files-between king rook)
+                                                                 (map (fn [file] (-> rook :coords (assoc :file file)))))]
                                           (when-not (some #(or (board %) (under-attack %)) (take 3 castle-spaces))
                                             (assoc rook :castle-spaces (rest castle-spaces)))))))]
     (when (:king castles)
@@ -229,15 +243,16 @@
                    {:game-result :draw}))))
 
 (comment
-  (get-all-valid-moves {"2A" {:piece :pawn :color :black :coords "2A"}
-                        "3B" {:piece :pawn :color :white :coords "3B"}
-                        "2C" {:piece :pawn :color :white :coords "2C"}
-                        "4D" {:piece :pawn :color :black :coords "4D"}
-                        "4E" {:piece :pawn :color :white :coords "4E"}
-                        "8F" {:piece :rook :color :black :coords "8F"}
-                        "1F" {:piece :king :color :white :coords "1F"}
-                        :turn :white
-                        :en-passante "4D"})
+  (get-all-valid-moves (normalize-board
+                        {"2A" {:piece :pawn :color :black :coords "2A"}
+                         "3B" {:piece :pawn :color :white :coords "3B"}
+                         "2C" {:piece :pawn :color :white :coords "2C"}
+                         "4D" {:piece :pawn :color :black :coords "4D"}
+                         "4E" {:piece :pawn :color :white :coords "4E"}
+                         "8F" {:piece :rook :color :black :coords "8F"}
+                         "1F" {:piece :king :color :white :coords "1F"}
+                         :turn :white
+                         :en-passante "4D"}))
   (apply-move initial-board (second (get-all-basic-moves initial-board :white)))
   (get-all-basic-moves initial-board :white)
   (get-all-valid-moves initial-board)
