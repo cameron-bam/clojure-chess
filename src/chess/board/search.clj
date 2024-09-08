798312fd80a027e9bd3ff6797aa5ecd5c06a079f
Cameron White <cameronwhite16@gmail.com>
2024-09-08 11:04:38 -0600

    Fixing negamax implementation, using zero-sum scoring

--- src/chess/board/search.clj
+++ src/chess/board/search.clj
@@ -1,7 +1,5 @@
 (ns chess.board.search
-  (:require [chess.board :refer [get-all-valid-moves take-turn initial-board] :as board]))
-
-(def negate (partial * -1))
+  (:require [chess.board :refer [get-all-valid-moves take-turn initial-board opposing-color] :as board]))
 
 (defn make-negamax-search [{:keys [get-children score-node]}]
   (letfn [(negamax-search
@@ -10,15 +8,15 @@
             ([node depth color-int state]
              (swap! state update :visited conj node)
              (if (= depth 0)
-               (assoc node :negamax-score (score-node node))
+               (assoc node :negamax-score (* color-int (score-node node)))
                (let [children (->> (get-children node)
                                    (filter (complement (:visited @state))))]
                  (if-not (seq children)
                    (assoc node :negamax-score (score-node node))
                    (let [{:keys [negamax-score] :as best-node}
                          (reduce (fn [{:keys [negamax-score] :as max} child]
-                                   (let [result (-> (negamax-search child (dec depth) (negate color-int) state)
-                                                    (update :negamax-score negate))]
+                                   (let [result (-> (negamax-search child (dec depth) (* -1 color-int) state)
+                                                    (update :negamax-score (partial * -1)))]
                                      (if (> (:negamax-score result) negamax-score) result max)))
                                  {:negamax-score Integer/MIN_VALUE} children)]
                      (assoc node :negamax-score negamax-score :best-node best-node)))))))]
@@ -27,7 +25,9 @@
 
 (comment
   (let [negamax-search (make-negamax-search {:get-children (fn [board] (->> board get-all-valid-moves (map #(take-turn board %))))
-                                             :score-node (fn [{:keys [score turn]}] (get score turn))})
+                                             :score-node (fn [{:keys [score turn]}] (->> [turn (opposing-color turn)]
+                                                                                         (map score)
+                                                                                         (apply -)))})
         state (atom {:visited #{}})
         move (time (-> (negamax-search initial-board 4 1 state) meta :last-move))]
     (prn "Nodes visited" (count (:visited @state)))
