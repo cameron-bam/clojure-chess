ac74bca3312e2880ea2455570f49587c65218940
Cameron White <cameronwhite16@gmail.com>
2024-09-12 01:41:16 -0600

    Storing the hash of the previous boards instead of the whole board

--- src/chess/board/search.clj
+++ src/chess/board/search.clj
@@ -1,21 +1,20 @@
 (ns chess.board.search
-  (:require [chess.board :refer [get-all-valid-moves take-turn initial-board score-for] :as board]
-            [clojure.pprint :as pprint]))
+  (:require [chess.board :refer [get-all-valid-moves take-turn initial-board score-for] :as board]))
 
-(defn make-search [{:keys [get-children score-node]}]
+(defn make-search [{:keys [get-children score-node sort-children]}]
   (letfn [(get-unvisited [node context]
             (->> (get-children node)
-                 (remove (:visited @context))))
+                 (remove (comp (:visited @context) hash-unordered-coll))))
           (search
            ([node depth]
             (search node depth true))
            ([node depth maximize?]
-            (search node depth maximize? (atom {:visited #{}})))
+            (search node depth maximize? (atom {:visited #{} :max-unvisited-children 0})))
            ([{::keys [alpha beta] :as node} depth maximize? context]
-            (swap! context update :visited conj node)
+            (swap! context update :visited conj (hash-unordered-coll node))
             (if (<= depth 0)
               (assoc node ::score (score-node node))
-              (let [children (->> (get-unvisited node context) (sort-by #(cond-> (score-node %) maximize? (* -1))))]
+              (let [children (->> (get-unvisited node context) (sort-children maximize?))]
                 (if-not (seq children)
                   (assoc node ::score (score-node node))
                   (let [best-node (reduce (fn [{::keys [score] :as best} child]
@@ -27,25 +26,30 @@
                                                 reduced)))
                                           {::score (if maximize? Integer/MIN_VALUE Integer/MAX_VALUE)}
                                           children)]
+                    (swap! context update :max-unvisited-children max (count children))
                     (assoc node ::score (::score best-node) :best-node best-node)))))))]
                      (comp :best-node search)))
 
 
 (defn search-board [board]
-  (let [search (make-search {:get-children (fn [board] (->> board get-all-valid-moves (map #(take-turn board %))))
-                             :score-node (partial score-for (:turn board))})
-        state (atom {:visited #{}})
+  (let [score-node (partial score-for (:turn board))
+        search (make-search {:get-children (fn [board] (->> board get-all-valid-moves (map #(take-turn board %))))
+                             :score-node score-node
+                             :sort-children (fn [maximize? children] (cond-> (sort-by score-node children) maximize? reverse))})
+        state (atom {:visited #{} :max-unvisited-children 0})
         move (time (-> (search board 4 true state) meta :last-move))]
     (prn "Nodes visited" (count (:visited @state)))
+    (prn "Max unvisited children" (:max-unvisited-children @state))
     move))
 
 (defn test-search [tree]
   (let [search (make-search {:get-children :children
-                             :score-node :score})
-        state (atom {:visited #{}})
-        move (time (-> (search tree 4 true state)))]
+                             :score-node :score
+                             :sort-children (fn [maximize? children] (cond-> (sort-by :score children) maximize? reverse))})
+        state (atom {:visited #{} :max-unvisited-children 0})
+        move (time (-> (search tree 3 true state)))]
     (prn "Nodes visited" (count (:visited @state)))
-    move))
+    (dissoc move :children :best-node)))
 
 (defn make-test-node []
   {:score (- (rand-int 10) 5)})
@@ -61,5 +65,6 @@
 
 (comment
   (search-board initial-board)
-  (test-search (doto (make-test-tree 5 2) pprint/pprint))
+  (test-search (make-test-tree 5 31))
+  (+ 1 1)
   )
