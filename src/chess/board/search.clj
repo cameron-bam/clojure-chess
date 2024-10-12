b8fdaa99fcb8661d51d27dd040eb4178ff6c6032
Cameron White <cameronwhite16@gmail.com>
2024-10-11 22:45:56 -0600

    Switching to defrecord for data storage

--- src/chess/board/search.clj
+++ src/chess/board/search.clj
@@ -1,34 +1,70 @@
 (ns chess.board.search
-  (:require [chess.board :refer [get-all-valid-moves take-turn initial-board score-for] :as board]))
+  (:require [chess.board :refer [get-all-valid-moves take-turn initial-board score-for] :as board])
+  (:import (java.util.concurrent Semaphore)))
 
-(defn make-search [{:keys [get-children score-node sort-children]}]
+
+(defn context->get-permits [max-permits context] 
+  (loop [acquired-permits 0]
+    (if (>= acquired-permits max-permits)
+      max-permits
+      (let [permits-to-aquire 1]
+        (if-not (-> @context :semaphore (.tryAcquire permits-to-aquire))
+          acquired-permits
+          (recur (+ acquired-permits permits-to-aquire)))))))
+
+(defn context->restore-permit [context]
+  (-> @context :semaphore .release))
+
+(defrecord ScoredMove [move score following-move])
+
+(defn make-search [{:keys [get-children score-node sort-children parallelism]}]
   (letfn [(get-unvisited [node context]
             (->> (get-children node)
-                 (remove (comp (:visited @context) hash-unordered-coll))))
+                 (remove (:visited @context))))
           (search
            ([node depth]
             (search node depth true))
            ([node depth maximize?]
-            (search node depth maximize? (atom {:visited #{} :max-unvisited-children 0})))
-           ([{::keys [alpha beta] :as node} depth maximize? context]
-            (swap! context update :visited conj (hash-unordered-coll node))
+            (search node depth maximize? (atom {})))
+           ([node depth maximize? context]
+            (search node depth maximize? context nil nil))
+           ([node depth maximize? context alpha beta]
+            (when (= @context {})
+              (swap! context merge {:visited #{} :semaphore (Semaphore. (or parallelism (* 2 (.availableProcessors (Runtime/getRuntime)))))}))
+            (swap! context update :visited conj node)
             (if (<= depth 0)
-              (assoc node ::score (score-node node))
+              (->ScoredMove node (score-node node) nil)
               (let [children (->> (get-unvisited node context) (sort-children maximize?))]
                 (if-not (seq children)
-                  (assoc node ::score (score-node node))
-                  (let [best-node (reduce (fn [{::keys [score] :as best} child]
-                                            (let [result (search (assoc child (if maximize? ::alpha ::beta) score) (dec depth) (not maximize?) context)
-                                                  {::keys [score] :as best} (if ((if maximize? > <) (::score result) score) result best)]
-                                              (cond-> best
-                                                (or (and maximize? (int? beta) (> score beta))
-                                                    (and (not maximize?) (int? alpha) (< score alpha)))
-                                                reduced)))
-                                          {::score (if maximize? Integer/MIN_VALUE Integer/MAX_VALUE)}
-                                          children)]
-                    (swap! context update :max-unvisited-children max (count children))
-                    (assoc node ::score (::score best-node) :best-node best-node)))))))]
-                     (comp :best-node search)))
+                  (->ScoredMove node (score-node node) nil)
+                  (let [best-node (loop [cur-best (->ScoredMove nil (if maximize? Integer/MIN_VALUE Integer/MAX_VALUE) nil)
+                                         children children]
+                                    (if (empty? children)
+                                      cur-best
+                                      (let [permits (context->get-permits (count children) context)
+                                            cur-best-score (.-score cur-best)
+                                            searches (max permits 1)
+                                            search-fn #(search %
+                                                               (dec depth)
+                                                               (not maximize?)
+                                                               context 
+                                                               (when maximize? cur-best-score)
+                                                               (when-not maximize? cur-best-score))
+                                            best-node (cond->> children
+                                                        :always (take searches)
+                                                        (> permits 0) (#(->> %
+                                                                             (mapv (fn [node] (future (let [res (search-fn node)]
+                                                                                                        (context->restore-permit context)
+                                                                                                        res))))
+                                                                             (map deref)))
+                                                        (<= permits 0) (map search-fn)
+                                                        :always (reduce #(try (if ((if maximize? > <) (.-score %1) (.-score %2)) %1 %2) (catch Exception e (prn %1 %2) (throw e))) cur-best))
+                                            score (.-score best-node)]
+                                        (recur best-node (when-not (or (and maximize? (int? beta) (> score beta))
+                                                                       (and (not maximize?) (int? alpha) (< score alpha)))
+                                                           (drop searches children))))))]
+                    (->ScoredMove node (.-score best-node) best-node)))))))]
+    (comp #(.-following-move %) search)))
 
 
 (defn search-board [board]
@@ -36,35 +72,39 @@
         search (make-search {:get-children (fn [board] (->> board get-all-valid-moves (map #(take-turn board %))))
                              :score-node score-node
                              :sort-children (fn [maximize? children] (cond-> (sort-by score-node children) maximize? reverse))})
-        state (atom {:visited #{} :max-unvisited-children 0})
-        move (time (-> (search board 4 true state) meta :last-move))]
+        state (atom {})
+        move (time (-> (search board 4 true state) :move meta :last-move))]
     (prn "Nodes visited" (count (:visited @state)))
-    (prn "Max unvisited children" (:max-unvisited-children @state))
     move))
 
-(defn test-search [tree]
-  (let [search (make-search {:get-children :children
-                             :score-node :score
-                             :sort-children (fn [maximize? children] (cond-> (sort-by :score children) maximize? reverse))})
-        state (atom {:visited #{} :max-unvisited-children 0})
-        move (time (-> (search tree 3 true state)))]
-    (prn "Nodes visited" (count (:visited @state)))
-    (dissoc move :children :best-node)))
+(defrecord TestNode [score children])
 
-(defn make-test-node []
-  {:score (- (rand-int 10) 5)})
+(defn test-search [tree]
+  (let [search (make-search {:get-children #(.-children %)
+                             :score-node #(.-score %)
+                             :sort-children (fn [maximize? children] (cond-> (sort-by #(.-score %) children) maximize? reverse))})
+        state (atom {})
+        move (time (-> (search tree 6 true state)))]
+    (prn "Nodes visited" (count (:visited @state)))
+    (-> move
+        (update :move dissoc :children)
+        (dissoc :following-move))))
+
+(defn make-test-node [children]
+  (->TestNode (- (rand-int 10) 5) children))
 
 (defn make-test-tree
   ([depth breadth]
-   (make-test-tree (make-test-node) depth breadth))
-  ([node depth breadth]
-   (cond-> node
-     (> depth 0)
-     (assoc :children (for [i (range 0 breadth)]
-                        (make-test-tree (make-test-node) (dec depth) breadth))))))
+   (make-test-node (if (<= depth 0) [] (for [_ (range 0 breadth)]
+                                         (make-test-tree (dec depth) breadth))))))
 
 (comment
   (search-board initial-board)
-  (test-search (make-test-tree 5 31))
-  (+ 1 1)
-  )
+
+  (def test-tree (make-test-tree 6 31))
+  (test-search test-tree)
+  (+ 1 1))
+
+
+(comment
+  (with-meta (TestNode 1 []) {:test 1}))
